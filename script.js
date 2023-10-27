a1726e39bf16bfd98e01a8736aab28f569df5f69
Davidson <davidsonjunior10@gmail.com>
2023-10-26 23:12:58 -0300

    Suffering. Trying to learn how to add text animation with canvas

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function testAnimation()
+var c = document.getElementById("canvas")
+var context = myCanvas.getContext("2d");
+var text = [];
+
+
+function getRandomNum(min, max) 
 {
-    
+    max = Math.floor
+    min = Math.ceil
+    Math.floor(Math.random)
 }
+
+context.font = "48px consolas";
+context.strokeText("Test",20,50);
